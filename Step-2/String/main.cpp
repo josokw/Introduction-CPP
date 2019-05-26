431ce0602fdf647e68581c921057da23e7a96b75
jos <jos.onokiewicz@han.nl>
2019-05-26 17:28:33 +0200

    Added more info texts

--- Step-2/String/main.cpp
+++ Step-2/String/main.cpp
@@ -9,25 +9,37 @@ using namespace std;
 
 int main()
 {
-   string str = "Hello C++ World"; // str is a string object
+   string str1 = "Hello C++ World"; // str is a string object
 
-   cout << str << endl;
+   cout << "1. str1 = " << str1 << endl;
 
-   str += " and a lot of developers";
-   cout << str << endl;
+   str1 += " and a lot of developers";
+   cout << "2. str1 = " << str1 << endl;
 
    // Can be used as an C char array and index operator []
-   str[10] = 'w';
-   cout << str << "  str[12] = " << str[12] << endl;
+   str1[10] = 'w';
+   cout << "3. str1 = " << str1 << ", str1[7] = " << str1[7] << endl;
 
-   str.clear();
-   str = "New text";
-   cout << str << "  " << str.substr(0, 3) << "  " << endl
-        << str.erase(0, 4) << endl;
-   cout << str << endl;
+   str1.clear();
+   // Assign C-style string to string object
+   str1 = "New text";
+   cout << "4. str1 = " << str1 << "  str1.substr(0, 3) = " << str1.substr(0, 3)
+        << "  " << endl;
+   str1.erase(0, 4);
+   cout << "5. str1 = " << str1 << endl;
 
-   str.replace(1, 2, "QWERTY");
-   cout << str << endl;
+   str1.replace(1, 2, "QWERTY");
+   cout << "6. str1 = " << str1 << endl;
+
+   string str2 = "Blah blah blah";
+   // Assign one string object to another
+   str2 = str1;
+   cout << "7. str2 = " << str2 << endl;
+
+   // Compare two strings
+   if (str1 == str2) {
+      cout << "8. str1 equals str2" << endl;
+   }
 
    return 0;
 }
